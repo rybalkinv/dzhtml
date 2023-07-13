c80e653e1dcec890252680eec9b7889103275f15
Vladimir <rybalkinv@inbox.ru>
2023-07-13 16:05:03 +0300

    done2

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+    let age = 101;
+    switch(age) {
+        case 18: {
+            alert("Вы совершеннолетний, все можно!");
+            break;
+        }
+        case 20: {
+            alert("Мужчина! А то и Женщина");
+            break;
+        }
+        default: {
+            alert("ой...");
+            break;
+        }    
+    }
+</script>
